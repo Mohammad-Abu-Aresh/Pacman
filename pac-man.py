9c8e014726645fc87160a84eeb298c0738bc2ea9
mabu-are <mabu-are@c1r5s8.irbid.42.school>
2026-09-07 18:14:19 +0300

    Resolve all flake8 and mypy issues

---
+++ pac-man.py
@@ -0,0 +1,13 @@
+# from package_pacman import ...
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        exit(1)
+    else:
+        exit(0)
