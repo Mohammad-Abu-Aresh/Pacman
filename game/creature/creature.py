cc886ad2f14562c3bd2c862f4a5e0b41773d6fd5
Mohammad Abu aresh <mhamadaresh@gmail.com>
2026-09-17 08:11:34 +0300

    fix: fixed the symtax for the creature file

--- game/creature/creature.py
+++ game/creature/creature.py
@@ -3,11 +3,10 @@ from abc import ABC, abstractmethod
 
 class Creature(ABC):
 
-    def __init__(self, spawn_point: tuple[int, int]) -> None:
+    def __init__(self, spawn_point: tuple[int, int]):
         self.x: int = spawn_point[0]
         self.y: int = spawn_point[1]
-        self.direction: int = 0  # 1: Up, 2: Right, 4: Down, 8: Left
-
+    
     @abstractmethod
     def move(self) -> None:
         pass
@@ -18,22 +17,21 @@ class Creature(ABC):
 
     @abstractmethod
     def die(self) -> None:
-        pass
+        ...
+
+    # def check_wall_collision() -> None:
+    #    pass
 
 
 class Player(Creature):
 
-    def __init__(
-            self, spawn_point: tuple[int, int],
-            super_timer: int = 0, lives: int = 3
-            ) -> None:
+    def __init__(self, spawn_point, lives: int = 3, super_timer: int = 15):
         super().__init__(spawn_point)
-        self._lives: int = lives
+        self._lives = lives
         self._score: int = 0
         self._speed: int = 100
-        self._is_super: bool = False
-        self._super_timer: int = super_timer
-        self._can_teleport: bool = False
+        self._is_super = False
+        self._super_timer = super_timer
 
     def move(self) -> None:
         pass
@@ -41,17 +39,14 @@ class Player(Creature):
     def draw(self) -> None:
         pass
 
-    def die(self) -> None:
-        pass
-
-    def handle_input(self, keys: list) -> None:
+    def handle_input(keys: list) -> None:
         pass
 
     def eat(self, item_type: str) -> int:
-        return 0
+        pass
 
     def respawn(self) -> None:
         pass
 
-    def update_speed(self, new_speed: int) -> None:
+    def update_speed(self, new_speed: int, time: int) -> None:
         self._speed = new_speed
